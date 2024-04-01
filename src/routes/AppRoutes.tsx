88433b75691781e9f49dd9ba5dd130a4fdb143fd
André Serafim <contato.andremoreira@gmail.com>
2024-04-01 12:30:25 -0300

    Importação das fontes do Material

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,15 +1,14 @@
+import { Button } from "@mui/material";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 const AppRoutes = () => {
 
   return (
     <Routes>
-        <Route path="/home" element={<p>Página Inicial</p>}/>
-
-        <Route path="*" element={<Navigate to="/home"/>} />  {/* O asterisco significa que se nenhuma rota for atendida, haverá um redirecionamento para a página inicial*/}
+        <Route path="/home" element={<Button>Teste</Button>}/>
+        <Route path="*" element={<Navigate to="/home"/>} />  {/* O asterisco significa que se nenhuma rota for atendida, haverá um redirecionamento para a página 'Home'*/}
     </Routes>
   )
-
 
 }
 
